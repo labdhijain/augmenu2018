b0e9dadc4ddd4d94b00e4c63e8afe09419d3a88e
labdhijain <karishmachhajer31@gmail.com>
2018-09-07 13:26:31 +0530

    change 1

--- k2.py
+++ k2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/pyhton2
 
 import psutil
-psutil.virtual_memory()
-psutil.cpu_times()
-psutil.swap_memory()
+c=psutil.virtual_memory()
+a=psutil.cpu_times()
+b=psutil.swap_memory()
 for x in range(3):
     psutil.cpu_times_percent(interval=1, percpu=False)
-
+print a
+print b
+print c
