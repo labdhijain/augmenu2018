ed9c260cc93ce5650ab36ae4bc2db57f694b9c9c
labdhijain <karishmachhajer31@gmail.com>
2018-09-07 13:29:53 +0530

    change 1

--- k4.py
+++ k4.py
@@ -5,8 +5,12 @@ def internet(host="8.8.8.8", port=53, timeout=3):
         try:
                 socket.setdefaulttimeout(timeout)
                 socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-                return True
+		return True
+		print running
+		
         except Exception as ex:
                 print ex.message
                 return False
 
+a=internet()
+print a
